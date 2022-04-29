134b23daf13ae5f943249e6499fdbcf501eaa810
FabioLaForesta <fabiolaforesta@icloud.com>
2022-04-29 15:36:50 +0200

    finish

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 const app = new Vue({
     el: '#app',
     data: {
-        email: ''
+        emails: []
     },
     methods: {},
     mounted() {
@@ -10,7 +10,9 @@ const app = new Vue({
                 //console.log(this);
                 //console.log(response);
                 //console.log(response.data.response);
-                this.email = response.data.response
+                for (let index = 0; index <= 10; index++) {
+                    this.emails.push(response.data.response)
+                }
             })
     }
 })
